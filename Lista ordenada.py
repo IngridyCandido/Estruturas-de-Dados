f8f1183cc3abfd07d77fd77b8350f2ea7012353b
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2026-03-19 15:19:23 -0300

    Add files via upload

--- Lista ordenada.py
+++ Lista ordenada.py
@@ -1,65 +1,54 @@
 import random
 
+def insertion_sort(lista):
+    for i in range(1, len(lista)):
+        key = lista[i]
+        j = i - 1
+
+        while j >= 0 and key < lista[j]:
+            lista[j + 1] = lista[j]
+            j -= 1
+
+        lista[j + 1] = key
+
+    return lista
+
+
+# Lista inicial
 numeros = [95, 1, 500, 20, 330, 40, 10, 200, 300, 50]
 
+print("Lista original:")
 print(numeros)
 
-def insertion_sort(numeros):
-    for i in range(1, len(numeros)):
-        key = numeros[i]
-        j = i - 1
-        while j >= 0 and key < numeros[j]:
-            numeros[j + 1] = numeros[j]
-            j -= 1
-        numeros[j + 1] = key
-    return numeros
+print("\nLista ordenada:")
+print(insertion_sort(numeros.copy()))
 
-print(insertion_sort(numeros))
 
+# Lista aleatória 1 (100 números)
 lista_aleatoria1 = random.sample(range(1000), 100)
 
+print("\nLista aleatória 1:")
 print(lista_aleatoria1)
 
-def insertion_sort(lista_aleatoria1):
-    for i in range(1, len(lista_aleatoria1)):
-        key = lista_aleatoria1[i]
-        j = i - 1
-        while j >= 0 and key < lista_aleatoria1[j]:
-            lista_aleatoria1[j + 1] = lista_aleatoria1[j]
-            j -= 1
-        lista_aleatoria1[j + 1] = key
-    return lista_aleatoria1
+print("\nLista aleatória 1 ordenada:")
+print(insertion_sort(lista_aleatoria1.copy()))
 
-print(insertion_sort(lista_aleatoria1))
 
+# Lista aleatória 2 (1000 números)
 lista_aleatoria2 = random.sample(range(10000), 1000)
 
+print("\nLista aleatória 2:")
 print(lista_aleatoria2)
 
-def insertion_sort(lista_aleatoria2):
-    for i in range(1, len(lista_aleatoria2)):
-        key = lista_aleatoria2[i]
-        j = i - 1
-        while j >= 0 and key < lista_aleatoria2[j]:
-            lista_aleatoria2[j + 1] = lista_aleatoria2[j]
-            j -= 1
-        lista_aleatoria2[j + 1] = key
-    return lista_aleatoria2
+print("\nLista aleatória 2 ordenada:")
+print(insertion_sort(lista_aleatoria2.copy()))
 
-print(insertion_sort(lista_aleatoria2))
 
+# Lista aleatória 3 (100000 números)
 lista_aleatoria3 = random.sample(range(1000000), 100000)
 
+print("\nLista aleatória 3:")
 print(lista_aleatoria3)
 
-def insertion_sort(lista_aleatoria3):
-    for i in range(1, len(lista_aleatoria3)):
-        key = lista_aleatoria3[i]
-        j = i - 1
-        while j >= 0 and key < lista_aleatoria3[j]:
-            lista_aleatoria3[j + 1] = lista_aleatoria3[j]
-            j -= 1
-        lista_aleatoria3[j + 1] = key
-    return lista_aleatoria3
-
-print(insertion_sort(lista_aleatoria3))
+print("\nLista aleatória 3 ordenada:")
+print(insertion_sort(lista_aleatoria3.copy()))
